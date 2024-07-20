916392c0b9541b60b71bf69d9f3dd0493bae9abe
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-20 18:10:54 +0300

    add Url.java

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,20 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+public class Url {
+    private Long id;
+    private String name;
+    private Timestamp createdAt;
+
+
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+}
