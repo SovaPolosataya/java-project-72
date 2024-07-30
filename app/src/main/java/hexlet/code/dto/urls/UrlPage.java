60079db58d6606d672d81ced8ce4943e7a6c82b3
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-30 05:35:13 +0300

    update files

--- app/src/main/java/hexlet/code/dto/urls/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -1,11 +1,18 @@
 package hexlet.code.dto.urls;
 
+import hexlet.code.dto.MainPage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
-public class UrlPage {
+public class UrlPage extends MainPage {
     private Url url;
+    private List<UrlCheck> urlChecks;
 }
--- app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlsPage.java
@@ -2,13 +2,16 @@ package hexlet.code.dto.urls;
 
 import hexlet.code.dto.MainPage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
-import java.util.List;
+import java.util.Map;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class UrlsPage extends MainPage {
-    private List<Url> urls;
+    private Map<Url, UrlCheck> mapChecks;
 }
