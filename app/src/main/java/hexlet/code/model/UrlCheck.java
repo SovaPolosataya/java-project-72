6697daa1e0575da63bd641ec898ad9a211ef885b
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-30 05:26:11 +0300

    add UrlCheck

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,38 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class UrlCheck {
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private Timestamp createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description, Long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
+
+    public UrlCheck(int statusCode, String title, String h1, String description) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+    }
+}
