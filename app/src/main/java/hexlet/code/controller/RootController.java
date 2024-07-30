e79b9c8d19b2d3d79c8f867f7ee095a78a527157
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-30 05:23:28 +0300

    add RootController

---
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -0,0 +1,17 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.MainPage;
+import io.javalin.http.Context;
+
+import java.util.Collections;
+
+public class RootController {
+
+    public static void welcome(Context ctx) {
+        var page = new MainPage();
+
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("index.jte", Collections.singletonMap("page", page));
+    }
+}
