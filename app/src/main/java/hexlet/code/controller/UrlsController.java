7961db8cef83dbd4725797fd42432ccd09f924ef
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-30 05:36:42 +0300

    delete Welcome

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -1,51 +1,52 @@
 package hexlet.code.controller;
 
-import hexlet.code.dto.MainPage;
 import hexlet.code.dto.urls.UrlPage;
 import hexlet.code.dto.urls.UrlsPage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
-
 import java.net.URI;
 import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class UrlsController {
 
-    public static void welcome(Context ctx) {
-        var page = new MainPage();
+    public static void allUrls(Context ctx) throws SQLException {
+        Map<Url, UrlCheck> mapChecks = new LinkedHashMap<>();
+        List<Url> urls = UrlRepository.getEntities();
+        Map<Long, UrlCheck> newChecks = UrlCheckRepository.findNewChecks();
 
+        for (Url url : urls) {
+            mapChecks.put(url, newChecks.get(url.getId()));
+        }
+
+        UrlsPage page = new UrlsPage(mapChecks);
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
-        ctx.render("index.jte", Collections.singletonMap("page", page));
-    };
-
-    public static void allUrls(Context ctx) throws SQLException {
-        var urls = UrlRepository.getEntities();
-        String flash = ctx.consumeSessionAttribute("flash");
-        String flashType = ctx.consumeSessionAttribute("flash-type");
-
-        var page = new UrlsPage(urls);
-        page.setFlash(flash);
-        page.setFlashType(flashType);
 
         ctx.render("urls/index.jte", model("page", page));
     }
 
     public static void showUrl(Context ctx) throws SQLException {
-        var id = ctx.pathParamAsClass("id", Long.class).get();
-        var url = UrlRepository.findId(id)
+        var id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Url url = UrlRepository.findId(id)
                 .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        List<UrlCheck> urlChecks = UrlCheckRepository.findUrlId(id);
 
-        var page = new UrlPage(url);
+        UrlPage page = new UrlPage(url, urlChecks);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
         ctx.render("urls/show.jte", model("page", page));
     }
 
@@ -55,7 +56,7 @@ public class UrlsController {
 
         try {
             assert inputUrl != null;
-            var uri = new URI(inputUrl);
+            URI uri = new URI(inputUrl);
             parsedUrl = uri.toURL();
         } catch (Exception e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
@@ -64,8 +65,10 @@ public class UrlsController {
             return;
         }
 
-        String name = parsedUrl.getProtocol() + "://" + parsedUrl.getAuthority();
-        var url = new Url(name, Timestamp.valueOf(LocalDateTime.now()));
+        String name = String.format("%s://%s%s", parsedUrl.getProtocol(), parsedUrl.getHost(),
+                        parsedUrl.getPort() == -1 ? "" : ":" + parsedUrl.getPort()).toLowerCase();
+
+        Url url = new Url(name, Timestamp.valueOf(LocalDateTime.now()));
 
         if (UrlRepository.findByName(name).isPresent()) {
             ctx.sessionAttribute("flash", "Страница уже существует");
