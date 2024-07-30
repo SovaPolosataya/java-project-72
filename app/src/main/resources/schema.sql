210e60cd07bbeb0c8853710d208b687d94e1073c
Irina Yukavskaya <strix1813@yandex.ru>
2024-07-30 05:32:04 +0300

    correcting DROP TABLE

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
+DROP TABLE IF EXISTS url_checks ;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE url_checks  (
+    id SERIAL PRIMARY KEY NOT NULL,
+    status_code bigint NOT NULL,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description text,
+    url_id bigint REFERENCES urls(id),
     created_at TIMESTAMP
 );
