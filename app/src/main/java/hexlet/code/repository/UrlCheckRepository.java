9e8359b0f0790b8fff9f470e8610889cc802e4e0
Irina Yukavskaya <strix1813@yandex.ru>
2024-08-06 21:18:01 +0300

    swapped the title and h1

--- app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
@@ -14,14 +14,14 @@ import java.util.Map;
 
 public class UrlCheckRepository extends BaseRepository {
     public static void save(UrlCheck urlCheck) throws SQLException {
-        var sql = "INSERT INTO url_checks (status_code, h1, title, description, url_id, created_at )"
+        var sql = "INSERT INTO url_checks (status_code, title, h1, description, url_id, created_at )"
                 + "VALUES (?, ?, ?, ?, ?, ?)";
 
         try (var conn = dataSource.getConnection();
              var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setInt(1, urlCheck.getStatusCode());
-            preparedStatement.setString(2, urlCheck.getH1());
-            preparedStatement.setString(3, urlCheck.getTitle());
+            preparedStatement.setString(2, urlCheck.getTitle());
+            preparedStatement.setString(3, urlCheck.getH1());
             preparedStatement.setString(4, urlCheck.getDescription());
             preparedStatement.setLong(5, urlCheck.getUrlId());
             preparedStatement.setTimestamp(6, urlCheck.getCreatedAt());
@@ -48,11 +48,11 @@ public class UrlCheckRepository extends BaseRepository {
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
                 int statusCode = resultSet.getInt("status_code");
-                String h1 = resultSet.getString("h1");
                 String title = resultSet.getString("title");
+                String h1 = resultSet.getString("h1");
                 String description = resultSet.getString("description");
                 Timestamp createdAt = resultSet.getTimestamp("created_at");
-                UrlCheck urlCheck = new UrlCheck(statusCode, h1, title, description, urlId, createdAt);
+                UrlCheck urlCheck = new UrlCheck(statusCode, title, h1, description, urlId, createdAt);
                 urlCheck.setId(id);
                 result.add(urlCheck);
             }
@@ -71,12 +71,12 @@ public class UrlCheckRepository extends BaseRepository {
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
                 int statusCode = resultSet.getInt("status_code");
-                String h1 = resultSet.getString("h1");
                 String title = resultSet.getString("title");
+                String h1 = resultSet.getString("h1");
                 String description = resultSet.getString("description");
                 Long urlId = resultSet.getLong("url_id");
                 Timestamp createdAt = resultSet.getTimestamp("created_at");
-                UrlCheck urlCheck = new UrlCheck(statusCode, h1, title, description, urlId, createdAt);
+                UrlCheck urlCheck = new UrlCheck(statusCode, title, h1, description, urlId, createdAt);
                 urlCheck.setId(id);
                 result.add(urlCheck);
             }
