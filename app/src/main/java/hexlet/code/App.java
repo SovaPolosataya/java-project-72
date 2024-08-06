70481bedf1a0e9b6f80e80c86d32d5a1c3c8c565
Irina Yukavskaya <strix1813@yandex.ru>
2024-08-06 19:11:59 +0300

    fixing tests

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -26,12 +26,11 @@ public final class App {
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "3000");
-        return Integer.valueOf(port);
+        return Integer.parseInt(port);
     }
 
     private static String getDatabase() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL",
-                "jdbc:h2:mem:project");
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
     }
 
     private static String readResourceFile(String fileName) throws IOException {
